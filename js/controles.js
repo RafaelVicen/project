ca318cca9e28f90647a3a7ae4805c972337ba873
Rafael Vicente <rafaelvic1946@gmail.com>
2023-06-01 13:20:48 +0100

    Nova versao do js

--- js/controles.js
+++ js/controles.js
@@ -14,7 +14,7 @@ function addTarefa() {
 
         <div class="item-botao">
           <button class="delete"><i class="mdi mdi-delete"></i> Deletar</button>
-        </div>
+        </div> 
       </div>`;
     main.innerHTML += novoItem;
   }
