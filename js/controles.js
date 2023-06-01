a064c1f2e83de4a7be43b03b7597a75f9ffdfd0a
Rafael Vicente <rafaelvic1946@gmail.com>
2023-06-01 13:05:24 +0100

    Novos controles javascript

---
+++ js/controles.js
@@ -0,0 +1,21 @@
+let input = document.getElementById("inputTarefa");
+let btnAdd = document.getElementById("btn-add");
+let main = document.getElementById("arealista");
+
+function addTarefa() {
+  let valorInput = input.value;
+  if (valorInput !== "" && valorInput !== null && valorInput !== undefined) {
+    let novoItem = `<div class="item">
+        <div class="item-icone">
+          <i class="mdi mdi-circle-outline"></i>
+        </div>
+
+        <div class="item-nome">Teste de tarefa</div>
+
+        <div class="item-botao">
+          <button class="delete"><i class="mdi mdi-delete"></i> Deletar</button>
+        </div>
+      </div>`;
+    main.innerHTML += novoItem;
+  }
+}
